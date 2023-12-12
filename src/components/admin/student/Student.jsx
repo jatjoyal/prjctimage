b9e07d86e66d4f73c0248c7392b2928b1741eb46
srutiap <135594255+srutiap@users.noreply.github.com>
2023-12-12 11:05:34 +0530

    12/12/23

--- src/components/admin/student/Student.jsx
+++ src/components/admin/student/Student.jsx
@@ -5,28 +5,70 @@ import { useNavigate } from 'react-router-dom'
 
 const Student = () => {
   const navigate =useNavigate();
-  var [inputs,setInputs] =useState({"Addmno":'',"name":'',"Age":'',"Course":''})
-   
+  var [inputs,setInputs] =useState({
+    "Addmno":'',
+    "name":'',
+    "Age":'',
+    "Course":''
+  });
+
+
+   var[selectedimage,setSelectedimage]=useState(null);
+
   const inputHandler = (event) =>{
     const{name,value}=event.target
     setInputs((inputs) => ({...inputs,[name]:value}))
     console.log(inputs)
   }
-  const addHandler =() =>{
-    console.log("clicked")
-    console.log(inputs)
-    axios.post("http://localhost:4000/new",inputs)
-    .then((response) =>{
-      alert("Record Saved")
-    })
-    .catch(err=>console.log(err))
+
+  const handleimage=(event)=>{
+    const file =event.target.files[0];
+    setSelectedimage(file)
+    inputs.image1=file;
+   }
+
+  // const addHandler =() =>{
+  //   console.log("clicked")
+  //   console.log(inputs)
+  //   axios.post("http://localhost:4000/new",inputs)
+  //   .then((response) =>{
+  //     alert("Record Saved")
+  //   })
+  //   .catch(err=>console.log(err))
   
-  }
+  // }
+
+   const saveData=()=>{
+    const formdata= new FormData();
+    formdata.append('Addmno',inputs.Addmno);
+    formdata.append('name',inputs.name);
+    formdata.append('Age',inputs.Age);
+    formdata.append('Course',inputs.Course);
+    formdata.append('image1',selectedimage);
+
+    fetch('http://localhost:4000/new',{
+      method:'post',
+    body:formdata,
+  })
+
+   .then((response)=>response.json())
+   .then((data)=>{
+
+    alert("record saved")
+   })
+
+    .catch((err)=>{
+      console.log("error")
+    })
+
+   }
+   
    
    const viewHandler =() =>
    {
     navigate('/studentview');
    }
+
   return (
     <div>
       <TextField label="Addmission Number" name="Addmno" value={inputs.Addmno} onChange={inputHandler} variant="standard" /><br></br>
@@ -41,11 +83,14 @@ const Student = () => {
     <MenuItem value={"BCA"}>BCA</MenuItem>
     <MenuItem value={"BCOM"}>BCOM</MenuItem>
     <MenuItem value={"BBA"}>BBA</MenuItem>
-  </Select><br></br><br></br>
-  <Button variant="contained" onClick={addHandler}>Submit</Button>
+  </Select><br></br>
+  <label>Choose file to upload</label>
+<input type="file" onChange={handleimage}></input>
+<br></br>
+  <Button variant="contained" onClick={saveData}>Submit</Button>
   <Button variant="contained" onClick={viewHandler}>view</Button>
     </div>
-  )
+  );
 }
 
 export default Student
